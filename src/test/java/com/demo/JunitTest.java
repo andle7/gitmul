440fd088488dd98bad479f4858ee03b5970e68a2
andle7 <hb1809@naver.com>
2023-05-30 14:48:14 +0900

    'workflows'

---
+++ src/test/java/com/demo/JunitTest.java
@@ -0,0 +1,13 @@
+package com.demo;
+import org.junit.Test;
+public class JunitTest {
+
+    @Test
+    public void getMulticampus() {
+
+            // 하나의 기능만 동작하는게 아니라
+                // 여러개의 기능이 들어가는 경우가 있음.
+
+        System.out.println("Multicampus");
+    }
+}
